dd0e924e932ae98f7d16a5752c599ac7e1aabd26
Josh Hamilton <joshamilton@gmail.com>
2017-08-10 11:17:56 -0500

    OTU tables based on 0.003 rel. abund. and 10% persistence

--- code/03bFilterOTUs.py
+++ code/03bFilterOTUs.py
@@ -23,6 +23,14 @@ import re
 
 deblurDir = '../results/deblur' # BIOM file from deblurring
 
+
+### Cutoffs. he previous analyses suggest that a relative abundance threshold 
+### of  0.003 and a persistence of 0.1 will capture > 80% of all sequences, 
+### while retaining over 50% of sequences in each sample. There will be ~ 100
+### OTUs.
+persist = 0.1
+abund = 0.003
+
 #%%#############################################################################
 ### Import otu table and calculate relative abundances
 ################################################################################
@@ -34,44 +42,7 @@ otuTable = pd.read_csv(deblurDir+'/otuTable.csv', sep=',', index_col=0)
 relAbundTable = otuTable / otuTable.sum(axis=0)
 
 #%%#############################################################################
-### Taken together, the previous analyses suggest that a relative abundance
-### threshold of  0.001 will capture > 90% of the total sequences and a
-### persistence threshold of 0.7 will capture > 80% of the sequences.
-###
-### Let's simplify the OTU table and write to file
-################################################################################
-
-## Absolute abundances
-# Replace all relative abundances below the threshhold and drop all-0 rows
-simpleOtuTable = otuTable.copy()
-simpleOtuTable[relAbundTable < 0.001] = 0
-simpleOtuTable = simpleOtuTable.loc[~(simpleOtuTable==0).all(axis=1)]
-
-## Relative abundances
-# Replace all relative abundances below the threshhold and drop all-0 rows
-simpleRelAbundTable = relAbundTable.copy()
-simpleRelAbundTable[simpleRelAbundTable < 0.001] = 0
-simpleRelAbundTable = simpleRelAbundTable.loc[~(simpleRelAbundTable==0).all(axis=1)]
-
-## Further filter based on persistence
-simpleOtuTable['Persistence'] = simpleOtuTable.astype(bool).sum(axis=1) / len(simpleOtuTable.columns)
-simpleOtuTable = simpleOtuTable[simpleOtuTable.Persistence < 0.7]
-simpleOtuTable = simpleOtuTable.drop('Persistence', axis=1)
-
-simpleRelAbundTable['Persistence'] = simpleRelAbundTable.astype(bool).sum(axis=1) / len(simpleRelAbundTable.columns)
-simpleRelAbundTable = simpleRelAbundTable[simpleRelAbundTable.Persistence > 0.7]
-simpleRelAbundTable = simpleRelAbundTable.drop('Persistence', axis=1)
-
-# Write to file
-simpleOtuTable.to_csv(deblurDir+'/simpleOtuTable.csv')
-simpleRelAbundTable.to_csv(deblurDir+'/simpleRelAbundTable.csv')
-
-#%%#############################################################################
-### Taken together, the previous analyses suggest that a relative abundance
-### threshold of  0.001 will capture > 90% of the total sequences. Around 10% 
-### of all sequence variants account for this 90%.
-###
-### Also compute a table of average relative abundances
+### Compute a filtered table of average relative abundances
 ################################################################################
 
 # Import table and sort
@@ -91,12 +62,12 @@ avgRelAbundTable.columns = sampleNames
 
 # Simplify average relative abundances
 simpleAvgRelAbundTable = avgRelAbundTable.copy()
-simpleAvgRelAbundTable[simpleAvgRelAbundTable < 0.001] = 0
+simpleAvgRelAbundTable[simpleAvgRelAbundTable < abund] = 0
 simpleAvgRelAbundTable = simpleAvgRelAbundTable.loc[~(simpleAvgRelAbundTable==0).all(axis=1)]
 
 ## Further filter based on persistence
 simpleAvgRelAbundTable['Persistence'] = simpleAvgRelAbundTable.astype(bool).sum(axis=1) / len(simpleAvgRelAbundTable.columns)
-simpleAvgRelAbundTable = simpleAvgRelAbundTable[simpleAvgRelAbundTable.Persistence > 0.7]
+simpleAvgRelAbundTable = simpleAvgRelAbundTable[simpleAvgRelAbundTable.Persistence > persist]
 simpleAvgRelAbundTable = simpleAvgRelAbundTable.drop('Persistence', axis=1)
 
 # Write to file
