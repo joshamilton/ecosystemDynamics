7c035c4cbcbdda04fc5e2d8cad5ba7d2b3b9c292
Josh Hamilton <joshamilton@gmail.com>
2017-09-19 16:18:17 -0500

    Check mean-variance scaling before filtering. Check homoskedasticity statistically.

--- code/03aRelAbundPersistence.py
+++ code/03aRelAbundPersistence.py
@@ -13,12 +13,14 @@
 ### Import packages
 ################################################################################
 
+import copy
 import math
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import re
+import statsmodels.stats.diagnostic as smd
 
 #%%#############################################################################
 ### Define folder structure
@@ -45,10 +47,13 @@ avgRelAbundTable = otuTable / otuTable.sum(axis=0)
 # Compute average for each sample (every 2 columns)
 avgRelAbundTable = avgRelAbundTable.groupby(np.arange(len(avgRelAbundTable.columns))//2, axis=1).mean()
 
-# Retrive the list of sample names and remove the '.R2'
+# Retrieve the list of sample names and remove the '.R2'
 sampleNames = list(otuTable.columns[::2])
 sampleNames = [re.sub('.R1', '', sample) for sample in sampleNames]
 avgRelAbundTable.columns = sampleNames
+
+# Write to file
+avgRelAbundTable.to_csv(deblurDir+'/avgRelAbundTable.csv')
 
 #%%#############################################################################
 ### Consider a range of relative abundances and persistences
@@ -165,3 +170,75 @@ plt.ylabel('Persistence')
 cbar = plt.colorbar(CS, ticks=yRange)
 
 plt.savefig(deblurDir+'/Filter on Persist and Rel Abund.png')
+
+#%%#############################################################################
+### Check mean-variance scaling: visualization
+################################################################################
+
+meanSeries = np.log10(avgRelAbundTable.mean(axis=1))
+varianceSeries = np.log10(avgRelAbundTable.var(axis=1))
+
+# Drop all OTUs w/ zero abundance
+meanSeries = meanSeries.loc[meanSeries != -np.inf]
+varianceSeries = varianceSeries.loc[varianceSeries != -np.inf]
+
+plt.figure(0) # create the figure
+plt.scatter(meanSeries, varianceSeries)
+plt.xlabel('Log10 Mean(OTU)')
+plt.ylabel('Log10 Variance(OTU)')
+
+plt.savefig(deblurDir+'/Mean-Variance Scaling.png')
+
+#%%#############################################################################
+### Check mean-variance scaling: statistical testing
+################################################################################
+
+# Null hypothesis is that the data are homoscedastic. That is, the variance is
+# constant. This can be tested by looking at the residuals: their standard
+# deviation should be constant and not depend on the x-value.
+
+# The  Breusch–Pagan test can be used to test for heteroskedasticity given
+# residuals from a fit. It is implemented in the statsmodels package.
+
+# Consider a range of cutoff values
+cutoffList = list(np.logspace(-3, -2, 21, base=10))
+pDF = pd.DataFrame(0.0, index=cutoffList, columns=['P-value'])
+
+for cutoff in cutoffList:
+
+    # Make a copy of the table
+    tempAvgRelAbundTable = copy.deepcopy(avgRelAbundTable)
+    
+    # Filter table, removing all OTUs with abundance < cutoff
+    tempAvgRelAbundTable[tempAvgRelAbundTable < cutoff] = 0
+    
+    # Compute mean and variance of each OTU
+    meanSeries = np.log10(tempAvgRelAbundTable.mean(axis=1))
+    varianceSeries = np.log10(tempAvgRelAbundTable.var(axis=1))
+    
+    # Drop all OTUs w/ zero abundance
+    meanSeries = meanSeries.loc[meanSeries != -np.inf]
+    varianceSeries = varianceSeries.loc[varianceSeries != -np.inf]
+    
+    # Regress variance on the mean
+    slope, intercept = np.polyfit(meanSeries, varianceSeries, 1)
+    
+    # Compute residuals
+    fitDF = pd.DataFrame(0, index=meanSeries.index, columns=['Mean', 'Variance', 'Estimate', 'Residual'])
+    fitDF['Mean'] = meanSeries
+    fitDF['Variance'] = varianceSeries
+    fitDF['Estimate'] = slope*fitDF['Mean'] + intercept
+    fitDF['Residual'] = fitDF['Estimate'] - fitDF['Variance']
+    
+    plt.figure(cutoffList.index(cutoff)) # create the figure
+    plt.scatter(fitDF['Mean'], fitDF['Residual'])
+    plt.xlabel('Log10 Mean(OTU)')
+    plt.ylabel('Residual')
+    fileName = 'Residuals of Mean-Variance Scaling-'+str(cutoff)+'.png'
+    plt.savefig(deblurDir+'/'+fileName)
+    
+    lm, lm_pvalue, fvalue, f_pvalue = smd.het_breuschpagan(fitDF['Residual'], fitDF['Mean'].to_frame())
+    
+    pDF.set_value(cutoff, 'P-value', f_pvalue)
+    
+pDF.to_csv(deblurDir+'/Heteroskedasticity.csv')
